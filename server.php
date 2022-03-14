d4dfe29d70bf24c55c6cd03d8e8d844db52c2d7d
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-14 18:49:28 +0100

    Creata la chiamata API

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once __DIR__ . '/./database.php';
+
+$response = [
+    'success' => true,
+    'response' => $discs
+];
+
+header('Content-Type: application/json');
+
+echo json_encode($response);
