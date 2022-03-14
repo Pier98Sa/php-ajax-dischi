59e907877b3246406118830391ad25f58f83167d
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-14 19:45:30 +0100

    Esercizio Bonus completato

--- script.js
+++ script.js
@@ -3,18 +3,37 @@ const root = new Vue(
         el: '#root',
 
         data:{
+            selected: "",
             listaAlbum: [],
+            listaGeneri: [],
             api: "http://localhost/php-ajax-dischi/server.php",
         },
+        computed: {
+            listaAlbumFiltrati(){
+                if(this.selected == ""){
+                    return this.listaAlbum;
+                }else {
+                    return this.listaAlbum.filter((elemento) =>{
+                        return elemento.genre == this.selected; 
+                    });
+                } 
+           },
+          },
         methods:{
             getAlbum(){
               axios.get(this.api)
                 .then((response) => {
                     //popolamento dell'array listaAlbum
                     this.listaAlbum = response.data.response;
-                    this.loadingInProgress = false;
+                    //popolamento dell'array listaGeneri
+                    this.listaAlbum.forEach((album) => {
+                        if(!this.listaGeneri.includes(album.genre)){
+                            this.listaGeneri.push(album.genre);
+                        }
+                    });
                 });
-            }, 
+            },
+
         },
         created(){
             this.getAlbum();
