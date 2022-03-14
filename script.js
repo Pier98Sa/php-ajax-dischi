ecc8b43ced8f7882708991703ce20b37c602dec2
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-14 19:17:21 +0100

    Milestone 2 completato

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ const root = new Vue(
         el: '#root',
 
         data:{
-            
+            listaAlbum: [],
+            api: "http://localhost/php-ajax-dischi/server.php",
         },
+        methods:{
+            getAlbum(){
+              axios.get(this.api)
+                .then((response) => {
+                    //popolamento dell'array listaAlbum
+                    this.listaAlbum = response.data.response;
+                    this.loadingInProgress = false;
+                });
+            }, 
+        },
+        created(){
+            this.getAlbum();
+        }
         
     }    
 );
