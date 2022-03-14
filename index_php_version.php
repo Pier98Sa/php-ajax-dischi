5de9a7e1278b1ba459852b343d603b9cbdc54800
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-03-14 16:57:40 +0100

    creata grafica statica

---
+++ index_php_version.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi</title>
+
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+
+    <header>
+        <img src="./img/Spotify_Logo.png" alt="">
+    </header>
+
+    <main>
+        <div class="container">
+            
+            <div class="box">
+                <div class="card">
+                    <div class="img-card">
+                        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+                    </div>
+
+                    <div class="info-card">
+                        <h2>New Jersey</h2>
+                        <div class="info-album">
+                            <h3>Bon Jovi</h3>
+                            <h3>1988</h3>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            
+            
+
+            
+            
+
+        </div>
+
+    </main>
+    
+</body>
+</html>
